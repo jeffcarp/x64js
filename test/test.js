ba98609e570f6c01e16c9efcb86d0901fb51a629
Jeff Carpenter <gcarpenterv@gmail.com>
2014-09-19 23:55:58 +0000

    Alphabetize test opcodes

--- test/test.js
+++ test/test.js
@@ -29,13 +29,6 @@ describe('x64', function() {
 
     describe('instructions:', function() {
 
-      describe('mov', function() {
-        it('should assign the rax register', function() {
-          cpu = x64.executeInstruction(cpu, 'mov rax, 5');
-          assert.equal(cpu.registers.rax, 5);
-        });
-      });
-
       describe('add', function() {
         it('adds src to dest by replacing dest', function() {
           cpu = x64.executeInstruction(cpu, 'mov rax, 7');
@@ -43,6 +36,13 @@ describe('x64', function() {
           cpu = x64.executeInstruction(cpu, 'add rax, rbx');
           assert.equal(cpu.registers.rax, 10);
           assert.equal(cpu.registers.rbx, 3);
+        });
+      });
+
+      describe('mov', function() {
+        it('should assign the rax register', function() {
+          cpu = x64.executeInstruction(cpu, 'mov rax, 5');
+          assert.equal(cpu.registers.rax, 5);
         });
       });
 
